e95972392613f3408c84a4a7092e886013d6fefd
rupikz <kydry.nikolau@gmail.com>
2021-12-30 21:55:15 +0300

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from './common/configs/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const configService = app.get(ConfigService);
+
+  const options = new DocumentBuilder().setTitle(configService.appName).build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
+  await app.listen(configService.port, configService.host);
+  console.log(`Application is running on: ${await app.getUrl()}/api`);
 }
 bootstrap();
