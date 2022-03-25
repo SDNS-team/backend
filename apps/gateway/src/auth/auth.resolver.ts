63068d06589ec9ed064fbef2e04cb6be3c2cbe60
rupikz <kydry.nikolau@gmail.com>
2022-03-25 23:26:14 +0300

    rename tokens model file

--- apps/gateway/src/auth/auth.resolver.ts
+++ apps/gateway/src/auth/auth.resolver.ts
@@ -3,7 +3,7 @@ import { Query, Resolver } from '@nestjs/graphql';
 import { User } from '@prisma/client/generated/user';
 import { AuthSession } from '../common/decorators/gql-auth.decorator';
 import { GqlRefreshGuard } from './token/guards/gql-refresh.guard';
-import { Tokens } from './token/models/token.model';
+import { Tokens } from './token/models/tokens.model';
 import { TokenService } from './token/token.service';
 
 @Resolver('auth')
